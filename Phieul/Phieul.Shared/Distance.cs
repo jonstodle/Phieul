1abca7490b139bdc73872d48193b1502119bdc36
Jon Stødle <jonstodle@outlook.com>
2014-05-14 22:50:36 +0200

    Corrected dump for 140514

---
+++ Phieul/Phieul.Shared/Distance.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phieul {
+    public struct Distance {
+        public enum Units { Kilometers, Miles, MetricMiles }
+        private readonly Units unit;
+        private readonly double value;
+
+        public Distance(double value, Units unit) {
+            this.unit = unit;
+           
+            double d = value;
+            switch(unit) {
+                case Units.Kilometers:
+                    break;
+                case Units.Miles:
+                    d *= 1.60934;
+                    break;
+                case Units.MetricMiles:
+                    d *= 10;
+                    break;
+                default:
+                    break;
+            }
+            this.value = d;
+        }
+
+        public Units Unit {
+            get { return unit; }
+        }
+
+        public double Value {
+            get { return value; }
+        }
+
+        public double CalculatedValue {
+            get {
+                double d = value;
+                switch(unit) {
+                    case Units.Kilometers:
+                        break;
+                    case Units.Miles:
+                        d *= 0.62137;
+                        break;
+                    case Units.MetricMiles:
+                        d *= 0.1;
+                        break;
+                    default:
+                        break;
+                }
+                return d;
+            }
+        }
+
+        public override string ToString() {
+            return CalculatedValue.ToString();
+        }
+
+        public static implicit operator Distance(double d) {
+            return new Distance(d, Units.Kilometers);
+        }
+
+        public static implicit operator double(Distance d){
+            return d.CalculatedValue;
+        }
+    }
+}
---
+++ Phieul/Phieul.Shared/Volume.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phieul {
+    public struct Volume {
+        public enum Units { Litres, Gallons }
+        private readonly Units unit;
+        private readonly double value;
+
+        public Volume(double value, Units unit) {
+            this.unit = unit;
+
+            double v = value;
+            switch(unit) {
+                case Units.Litres:
+                    break;
+                case Units.Gallons:
+                    v *= 3.78541;
+                    break;
+                default:
+                    break;
+            }
+            this.value = v;
+        }
+
+        public Units Unit {
+            get { return unit; }
+        }
+
+        public double Value {
+            get { return value; }
+        }
+
+        public double CalculatedValue {
+            get {
+                double v = value;
+                switch(unit) {
+                    case Units.Litres:
+                        break;
+                    case Units.Gallons:
+                        v *= 0.26417;
+                        break;
+                    default:
+                        break;
+                }
+                return v;
+            }
+        }
+
+        public override string ToString() {
+            return CalculatedValue.ToString();
+        }
+
+        public static implicit operator Volume(double d) {
+            return new Volume(d, Units.Litres);
+        }
+
+        public static implicit operator double(Volume v) {
+            return v.CalculatedValue;
+        }
+    }
+}
